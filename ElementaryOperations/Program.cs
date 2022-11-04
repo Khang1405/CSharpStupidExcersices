e5bf6c27d8995955152378146450a815433e25d6
Khang1405 <108504628+Khang1405@users.noreply.github.com>
2022-11-04 21:04:05 +0700

    non

---
+++ ElementaryOperations/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EX3
+{
+    class Program
+    {
+        static void ElementaryOperations(int a, int b)
+        {
+            if( b == 0)
+            {
+                Console.WriteLine("Deo chia duoc!!");
+            }
+            else
+            {
+                Console.WriteLine($"{a+b}, {a-b} , {a*b}, {(float)a/ (float)b}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            ElementaryOperations(3, 8);
+        }
+    }   
+}
