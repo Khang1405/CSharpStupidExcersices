bc3db8bc38e02d8029c818e580135904ae11d42f
Khang1405 <108504628+Khang1405@users.noreply.github.com>
2022-11-04 20:51:27 +0700

    too ezZ

---
+++ CelsiustoFahrenheit/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EX2
+{
+    class Program
+    {
+        static void CtoF (double c)
+        {
+            if (c < -271.15) 
+            {
+                Console.WriteLine("Temperature below absolute zero!");
+            }
+            else
+            {
+                Console.WriteLine($"T = {(c * 1.8) + 32}F");
+            }
+        }
+        static void Main(string[] args)
+        {
+            CtoF(0);
+            CtoF(100);
+            CtoF(-300);
+        } 
+    }
+}
