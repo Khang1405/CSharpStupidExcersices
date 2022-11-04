a4d2f11a5184cb67392b46240b1b5fd5ef225ac8
Khang1405 <108504628+Khang1405@users.noreply.github.com>
2022-11-04 20:35:34 +0700

    two ez

---
+++ addtwo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EX1
+{
+    class Program
+    {
+        public static float AddAndMultiply(float a, float b, float c)
+        {
+            return (a + b) * c;
+        }
+    
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddAndMultiply(2,4,5));
+        }
+    }
+}
